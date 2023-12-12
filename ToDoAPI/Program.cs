659c578b393003bb48f9d3c2cb597360252bba73
Ventsislav Kalapchiyski <yavorov1995@gmail.com>
2023-12-12 16:39:32 +0200

    project setup for entity framework and db creation

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ToDoAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default connection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
