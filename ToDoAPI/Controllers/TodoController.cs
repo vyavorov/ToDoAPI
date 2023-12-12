699ad8b4e3357f17d860a5d9677a5dfa04e72ecc
Ventsislav Kalapchiyski <yavorov1995@gmail.com>
2023-12-12 16:39:32 +0200

    project setup for entity framework and db creation

---
+++ ToDoAPI/Controllers/TodoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoAPI.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ToDoAPI/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAPI.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = null!;
+
+        public bool isCompleted { get; set; }
+    }
+}
