7ab8e19af0cdac2b30bc94cbf790ccdba52f8b6c
Ventsislav Kalapchiyski <yavorov1995@gmail.com>
2023-12-13 09:37:18 +0200

    controller finished

---
+++ ToDoAPI/Data/TodoSeeder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Models;
+
+namespace ToDoAPI.Data;
+
+public static class TodoSeeder
+{
+    public static void Seed(IServiceProvider serviceProvider)
+    {
+        using (var context = new AppDbContext(serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>()))
+        {
+            context.Database.EnsureCreated();
+
+            if (!context.Todos.Any())
+            {
+                context.Todos.AddRange(
+                    new Todo { Title = "Buy groceries", isCompleted = false },
+                    new Todo { Title = "Write code", isCompleted = false }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
