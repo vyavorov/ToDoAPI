8e29f3d1731f0582e08e6334544e62b03deddc21
Ventsislav Kalapchiyski <yavorov1995@gmail.com>
2024-02-19 13:09:11 +0200

    work factor added to password hashing

--- ToDoAPI/Services/PasswordHashService.cs
+++ ToDoAPI/Services/PasswordHashService.cs
@@ -4,9 +4,10 @@ namespace ToDoAPI.Services;
 
 public class PasswordHashService : IPasswordHashService
 {
+    private const int WorkFactor = 12;
     public string HashPassword(string password)
     {
-        return BCrypt.Net.BCrypt.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt());
+        return BCrypt.Net.BCrypt.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt(WorkFactor));
     }
 
     public bool VerifyPassword(string providedPassword, string storedPasswordHash)
