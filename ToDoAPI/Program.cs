a991bdce4df0bbbdca95d97f6139145b1ed33805
Ventsislav Kalapchiyski <yavorov1995@gmail.com>
2024-02-20 11:55:11 +0200

    added procfile for heroku deployment

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -64,6 +64,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
